eb0adfa7a3d531b6a2cbef10f1f6095587df0dd8
Karthik Kundurthy <karthikkundurthy@gmail.com>
2024-09-28 12:36:38 -0400

    openai setup

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-import lanchain
+import langchain
 from openai import OpenAI
+from langchain_openai import OpenAIEmbeddings
+import secrets as s
+
+embeddings_model = OpenAIEmbeddings(api_key=s.api_key)
 
 import os
 
-client = OpenAI(api_key=s.api_key)
-completion = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {"role": "user", "content": "write a haiku about ai"}
-    ], 
-    
-)
 
-# langchain vectorize
+
+
+def get_openai_client():
+    client = OpenAI(api_key=s.api_key)
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": "write a haiku about ai"}
+        ], 
+        
+    )
+
+    return client
+
+embeddings = embeddings_model.embed_documents(
+    [
+        "Hi there!",
+        "Oh, hello!",
+        "What's your name?",
+        "My friends call me World",
+        "Hello World!"
+    ]
+)
+# print(len(embeddings), len(embeddings[0]))
