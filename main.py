81883afa4ade9f66330800ea8f541c66f1732a7d
Karthik Kundurthy <karthikkundurthy@gmail.com>
2024-09-28 12:59:28 -0400

    initialized mongodb cluster

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import langchain
 from openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
 import secrets as s
+from pymongo.mongo_client import MongoClient
+
 
 embeddings_model = OpenAIEmbeddings(api_key=s.api_key)
 
@@ -31,4 +33,20 @@ embeddings = embeddings_model.embed_documents(
         "Hello World!"
     ]
 )
+
+
+
+uri = "mongodb+srv://karthikkundurthy:P978MHSWhtycWRC8@voicemirrorvectordb.a3on0.mongodb.net/?retryWrites=true&w=majority&appName=voiceMirrorVectorDB"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
 # print(len(embeddings), len(embeddings[0]))
